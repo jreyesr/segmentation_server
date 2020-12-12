45e00727b06a98b65735d9ccad122f3da3fc3a68
jreyesr <jreyesr@users.noreply.github.com>
2020-12-12 10:41:30 -0500

    Add Linux/Windows switching on commands

--- commands.py
+++ commands.py
@@ -1,10 +1,19 @@
 import os
 import shutil
+import platform
+
+def _call(command):
+    plat = platform.system()
+    assert (plat in ("Linux, Windows")), "OS {} not supported".format(plat)
+    if plat == "Windows":
+        return os.system("ubuntu2004 {}".format(command))
+    elif plat == "Linux":
+        return os.system(command)
 
 def to_pcd(pointcloud):
     print("LAS -> PCD")
     if not pointcloud.istype(".pcd"): # only convert to PCD if it isn't already
-        ret = os.system("pdal translate -i pointclouds/{} -o {}".format(pointcloud.filename, pointcloud.pcd_path))
+        ret = _call("pdal translate -i pointclouds/{} -o {}".format(pointcloud.filename, pointcloud.pcd_path))
     else: # fake successful command invocation
         ret = 0
     print("LAS -> PCD complete")
@@ -18,7 +27,7 @@ def remove_ground(pointcloud):
 
 def to_las(pointcloud):
     print("PCD -> LAS")
-    ret = os.system("pdal translate -i {} -o {}".format(pointcloud.final_pcd_path, pointcloud.final_las_path))
+    ret = _call("pdal translate -i {} -o {}".format(pointcloud.final_pcd_path, pointcloud.final_las_path))
     print("PCD -> LAS complete")
     return ret == 0
 
@@ -30,6 +39,6 @@ def to_potree(pointcloud, original=False):
     else:
         src = pointcloud.final_las_path
         dst = pointcloud.uid
-    ret = os.system("./PotreeConverter {} -o potree/pointclouds/{}".format(src, dst))
+    ret = _call("./PotreeConverter {} -o potree/pointclouds/{}".format(src, dst))
     print("To Potree format complete")
     return ret == 0
